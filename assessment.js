243b33acab0b60ddcdb7386d1d41e9a6ea32eba2
Kei-myon <Keimyon1009@outlook.jp>
2024-12-31 16:29:51 +0900

    ページデザインの変更

--- assessment.js
+++ assessment.js
@@ -15,14 +15,28 @@ assessmentButton.addEventListener(
     
     // 診断結果表示エリアの作成
     resultDivison.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivison.appendChild(header);
+    
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivison.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivison.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivison.appendChild(headerDivision);
+    resultDivison.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
